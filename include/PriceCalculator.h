f1a21edf903be86a4d647f4b454892e16061358b
SSK015 <18252677381@163.com>
2023-11-23 20:25:34 +0800

    Use Singleton Pattern to avoid repeat production of Discount

--- include/PriceCalculator.h
+++ include/PriceCalculator.h
@@ -2,7 +2,8 @@
 
 #include <cmath>
 #include <memory>
-
+#include <functional>
+#include <iostream>
 enum class DiscountType
 {
     CASH_NORMAL,
@@ -14,53 +15,108 @@ enum class DiscountType
 
 namespace PriceCalc
 {
+    class Discount
+    {
+    public:
+        virtual double AcceptCash(const double money) const noexcept = 0;
+        virtual ~Discount() = default;
+    };
+
+    class Normal final : public Discount
+    {
+    public:
+        static Normal &getInstance()
+        {
+            static Normal instance;
+            return instance;
+        }
+        double AcceptCash(const double money) const noexcept override
+        {
+            return money;
+        }
+
+    private:
+        Normal()
+        {
+            std::cout << "Construct Function of Normal" << std::endl;
+        }
+    };
+
+    class PercentOff final : public Discount
+    {
+    public:
+        static PercentOff &getInstance()
+        {
+            static PercentOff instance;
+            return instance;
+        }
+
+        void setRate(double newRate)
+        {
+            rate = newRate;
+        }
+
+        double AcceptCash(const double money) const noexcept override
+        {
+            return money * rate;
+        }
+
+    private:
+        double rate;
+        PercentOff() : rate(1.0)
+        {
+            std::cout << "Construct Function of PercentOff" << std::endl;
+        }
+    };
+
+    class CashBack final : public Discount
+    {
+    public:
+        static CashBack &getInstance()
+        {
+            static CashBack instance;
+            return instance;
+        }
+        double AcceptCash(const double money) const noexcept override
+        {
+            const double threshold = 100.0;
+            const double cashback = 20.0;
+            return money - std::floor(money / threshold) * cashback;
+        }
+
+    private:
+        CashBack()
+        {
+            std::cout << "Construct Function of CashBack" << std::endl;
+        }
+    };
+
     class PriceCalculator final
     {
     public:
-        double AcceptCash(const DiscountType discountType, const double money) const noexcept;
-
-    private:
-        class Discount
+        double AcceptCash(const DiscountType discountType, const double money) const noexcept
         {
-        public:
-            virtual double AcceptCash(const double money) const noexcept = 0;
-            virtual ~Discount() = default;
-        };
-        class Normal final : public Discount
-        {
-        public:
-            double AcceptCash(const double money) const noexcept override
+            switch (discountType)
             {
-                return money;
-            };
-        };
-        class PercentOff final : public Discount
-        {
-        public:
-            explicit PercentOff(const double rate) : rate(rate)
-            {
+            case DiscountType::CASH_NORMAL:
+                return Normal::getInstance().AcceptCash(money);
+            case DiscountType::CASH_PERCENTOFF_10:
+                PercentOff::getInstance().setRate(0.9);
+                return PercentOff::getInstance().AcceptCash(money);
+            case DiscountType::CASH_PERCENTOFF_20:
+                PercentOff::getInstance().setRate(0.8);
+                return PercentOff::getInstance().AcceptCash(money);
+            case DiscountType::CASH_PERCENTOFF_30:
+                PercentOff::getInstance().setRate(0.7);
+                return PercentOff::getInstance().AcceptCash(money);
+            case DiscountType::CASH_BACK:
+                return CashBack::getInstance().AcceptCash(money);
+            default:
+                return Normal::getInstance().AcceptCash(money);
             }
-            double AcceptCash(const double money) const noexcept override
-            {
-                // const double discountRate = 0.9;
-                return money * rate;
-            };
-
-        private:
-            double rate;
-        };
-        class CashBack final : public Discount
+        }
+        PriceCalculator()
         {
-        public:
-            double AcceptCash(const double money) const noexcept override
-            {
-                const double threshold = 100.0;
-                const double cashback = 20.0;
-
-                double cash = money - std::floor(money / threshold) * cashback;
-                // break;
-                return cash;
-            };
-        };
+        }
     };
 } // namespace PriceCalc
