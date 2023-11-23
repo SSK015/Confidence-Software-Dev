72eb8f970b8212bd88f270485db10e5ef25c56d7
SSK015 <18252677381@163.com>
2023-11-23 20:59:58 +0800

    Separate declaririon and implementation

--- include/PriceCalculator.h
+++ include/PriceCalculator.h
@@ -16,116 +16,48 @@ enum class DiscountType
 
 namespace PriceCalc
 {
-    class IDiscountStrategy
+    class Strategy
     {
     public:
         virtual double AcceptCash(const double money) const = 0;
-        virtual ~IDiscountStrategy() = default;
+        virtual ~Strategy() = default;
     };
 
-    class NormalStrategy final : public IDiscountStrategy
+    class NormalStrategy final : public Strategy
     {
     public:
-        static NormalStrategy &getInstance()
-        {
-            static NormalStrategy instance;
-            return instance;
-        }
-
-        double AcceptCash(const double money) const override
-        {
-            return money;
-        }
+        static NormalStrategy &getInstance();
+        double AcceptCash(const double money) const override;
 
     private:
-        NormalStrategy()
-        {
-            std::cout << "Construct Function of Normal!" << std::endl;
-        };
+        NormalStrategy();
     };
 
-    class PercentOffStrategy final : public IDiscountStrategy
+    class PercentOffStrategy final : public Strategy
     {
     public:
-        static PercentOffStrategy &getInstance()
-        {
-            static PercentOffStrategy instance;
-            return instance;
-        }
-
-        void setRate(double newRate)
-        {
-            rate = newRate;
-        }
-
-        double AcceptCash(const double money) const override
-        {
-            return money * rate;
-        }
+        static PercentOffStrategy &getInstance();
+        void setRate(double newRate);
+        double AcceptCash(const double money) const override;
 
     private:
         double rate;
-        PercentOffStrategy() : rate(1.0)
-        {
-            std::cout << "Construct Function of PercentOff!" << std::endl;
-        }
+        PercentOffStrategy();
     };
 
-    class CashBackStrategy final : public IDiscountStrategy
+    class CashBackStrategy final : public Strategy
     {
     public:
-        static CashBackStrategy &getInstance()
-        {
-            static CashBackStrategy instance;
-            return instance;
-        }
-
-        double AcceptCash(const double money) const override
-        {
-            const double threshold = 100.0;
-            const double cashback = 20.0;
-            return money - std::floor(money / threshold) * cashback;
-        }
+        static CashBackStrategy &getInstance();
+        double AcceptCash(const double money) const override;
 
     private:
-        CashBackStrategy()
-        {
-            std::cout << "Construct Function of CashBack" << std::endl;
-        };
+        CashBackStrategy();
     };
 
     class PriceCalculator final
     {
     public:
-        double AcceptCash(const DiscountType discountType, const double money) const
-        {
-            const IDiscountStrategy *strategy;
-            switch (discountType)
-            {
-            case DiscountType::CASH_NORMAL:
-                strategy = &NormalStrategy::getInstance();
-                break;
-            case DiscountType::CASH_PERCENTOFF_10:
-                PercentOffStrategy::getInstance().setRate(0.9);
-                strategy = &PercentOffStrategy::getInstance();
-                break;
-            case DiscountType::CASH_PERCENTOFF_20:
-                PercentOffStrategy::getInstance().setRate(0.8);
-                strategy = &PercentOffStrategy::getInstance();
-                break;
-            case DiscountType::CASH_PERCENTOFF_30:
-                PercentOffStrategy::getInstance().setRate(0.7);
-                strategy = &PercentOffStrategy::getInstance();
-                break;
-            case DiscountType::CASH_BACK:
-                strategy = &CashBackStrategy::getInstance();
-                break;
-            default:
-                strategy = &NormalStrategy::getInstance();
-                break;
-            }
-
-            return strategy->AcceptCash(money);
-        }
+        double AcceptCash(const DiscountType discountType, const double money) const;
     };
 }
